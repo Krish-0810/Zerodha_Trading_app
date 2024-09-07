9ac56ffe79d23f439401d46d539faabe9f914e79
krish <krishpatel85195@gmail.com>
2024-09-07 12:31:22 +0530

    Added fucntoinalities of forntend

---
+++ Frontend/vite-project/src/landingPage/about/About.jsx
@@ -0,0 +1,12 @@
+import Team from "./Team";
+import Hero from "./Hero";
+
+function About(){
+
+    return(<>
+        <Hero/>
+        <Team/>
+    </>)
+}
+
+export default About;
---
+++ Frontend/vite-project/src/landingPage/home/Homepage.jsx
@@ -0,0 +1,19 @@
+import Awards from "./Awards";
+import Education from "./Educaton";
+import Hero from "./Hero";
+import Pricing from "./Pricing";
+import Stats from "./Stats";
+
+function HomePage() {
+    return (
+      <>
+            <Hero/>
+            <Awards/>
+            <Education/>
+            <Pricing/>
+            <Stats/>
+      </>
+    );
+  }
+  
+  export default HomePage;
